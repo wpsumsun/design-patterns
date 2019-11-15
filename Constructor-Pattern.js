d8ab0037da261f06650083097c13cc4f05f6cf65
wangpeng <summer.wang@puman.com>
2019-11-16 01:00:43 +0800

    add constructor pattern introduction

--- Constructor-Pattern.js
+++ Constructor-Pattern.js
@@ -1,3 +1,9 @@
+/*
+This is a class-based creational design pattern. Constructors are special functions that can be used to instantiate new objects with methods and properties defined by that function.
+It is not one of the classic design patterns. In fact, it is more of a basic language construct than a pattern in most object-oriented languages. But in JavaScript, objects can be created on the fly without any constructor functions or “class” definition. Therefore, I think it is important to lay down the foundation for other patterns to come with this simple one.
+Constructor pattern is one of the most commonly used patterns in JavaScript for creating new objects of a given kind.
+*/
+
 // traditional Function-based syntax
 function Hero(name, specialAbility) {
   this.name = name;
